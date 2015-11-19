062eb341573d1f150b7318f29ebc89a8361b7da8
paopao <peeyada.e@thaiplastics.org>
2015-11-19 14:20:27 +0700

    Bind Widget Finish

--- app/src/main/java/apppith/pao/pithtraffic/MainActivity.java
+++ app/src/main/java/apppith/pao/pithtraffic/MainActivity.java
@@ -2,12 +2,31 @@ package apppith.pao.pithtraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private Button aboutMeButton;
+    private ListView trafficListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWiddget();
+
+
+    } // Main Class
+
+    private void bindWiddget() {
+
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+        trafficListView = (ListView) findViewById(R.id.listView);
     }
-}
+
+} // Main Class  นี่คือ คลาสหลัก
